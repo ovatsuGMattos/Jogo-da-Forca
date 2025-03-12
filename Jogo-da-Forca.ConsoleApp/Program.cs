95d9837fdec85a0d026f97abea0d6f49ac53728f
ovatusGMattos <gustavo.j.mattos1@gmail.com>
2025-03-12 15:35:40 -0300

    Implementação Jogo da forca

--- Jogo-da-Forca.ConsoleApp/Program.cs
+++ Jogo-da-Forca.ConsoleApp/Program.cs
@@ -4,7 +4,55 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+            string palavraescolhida = "MELANCIA";
+
+            char[] letrasEncontradas = new char[palavraescolhida.Length];
+
+            for (int caractereAtual = 0; caractereAtual < letrasEncontradas.Length; caractereAtual++) 
+            {
+                letrasEncontradas[caractereAtual] = '_';
+            }
+                        
+              do //faça
+            {
+                string letrasEncontradasCompleta = String.Join("", letrasEncontradas);
+                
+                Console.Clear();
+                Console.WriteLine("-----------------");
+                Console.WriteLine("Jogo da Forca");
+                Console.WriteLine("-----------------");
+                Console.WriteLine("Palavra escolhida: " + letrasEncontradasCompleta);
+                Console.WriteLine("-----------------");
+
+
+                //dado caractere
+
+                Console.WriteLine("Digite uma letra válida: ");
+                string entradainicial = Console.ReadLine()!.ToUpper();
+
+                if (entradainicial.Length > 1)
+                {
+                    Console.WriteLine("Informe apenas um caractere.");
+                    return;
+
+                }
+                char chute = entradainicial[0];
+
+                for (int contadorCaractere = 0; contadorCaractere < palavraescolhida.Length; contadorCaractere++)
+                {
+                    char caractereAtual = palavraescolhida[contadorCaractere];
+
+                    if (chute == caractereAtual)
+                    {
+                        letrasEncontradas[contadorCaractere] = caractereAtual;
+                    }
+                                    
+                }
+
+                Console.WriteLine(chute);
+
+                Console.ReadLine();
+            } while (true); //enquanto (condição)
         }
     }
 }
